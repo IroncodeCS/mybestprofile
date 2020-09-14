b712fd959f8a4eb6f4e38dcf8c4217912dba11b2
sarayut khamkhiao <sarayut@thinknet.co.th>
2020-09-14 22:31:22 +0700

    init project

---
+++ components/Nav.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav>
+        <div>User-Name</div>
+        <div>
+            <ul>
+                <li><a href="#"></a></li>
+            </ul>
+        </div>
+    </nav>
+</template>
